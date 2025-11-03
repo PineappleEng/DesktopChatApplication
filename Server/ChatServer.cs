3add9ba98775a3c8eb27a9da6a07ef07db7c7ff4
PineappleEng <derek.pib@gmail.com>
2025-11-02 16:22:27 -0800

    feat: added basic server setup

--- Server/ChatServer.cs
+++ Server/ChatServer.cs
@@ -1,17 +1,61 @@
-﻿using Server.Database;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
+
+using Common.Models;
+using Common.Network;
+using Server.Database;
 
 namespace Server
 {
     internal class ChatServer
     {
-        static void Main(string[] args)
+        private static 
+            ConcurrentDictionary<TcpClient, StreamWriter> clients 
+            = new ConcurrentDictionary<TcpClient, StreamWriter>();
+        static async Task Main()
         {
-            ChatDatabase db = new ChatDatabase();
+            ChatDatabase database = new ChatDatabase();
+            IPAddress localAddr = IPAddress.Parse("10.0.2.15");
+            int port = 6969;
+
+            TcpListener server = new TcpListener(localAddr, port);
+            server.Start();
+            Console.Write($"Chat Server Listening on {port}\r\n");
+
+            while (true)
+            {
+                TcpClient client = await server.AcceptTcpClientAsync();
+                Console.Write($"Client Connected: {client.Client.RemoteEndPoint}\r\n");
+                _ = Task.Run(() => HandleClient(client));
+            }
+        }
+
+        static async Task HandleClient(TcpClient client)
+        {
+            try
+            {
+                using (NetworkStream stream = client.GetStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+                    clients.TryAdd(client, writer);
+                    // I'll write this later cause I'm lazy
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.Write($"Error with {client.Client.RemoteEndPoint}: {ex.Message}\r\n");
+            }
+            finally
+            {
+                clients.TryRemove(client, out _);
+                client.Close();
+                Console.Write($"Client disconnected: {client.Client.RemoteEndPoint}\r\n");
+            }
         }
     }
 }
