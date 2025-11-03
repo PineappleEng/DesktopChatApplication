1630014d759228cff5c6af6da088f7eefce0ec6b
PineappleEng <derek.pib@gmail.com>
2025-11-02 22:40:53 -0800

    refactor: changed the name of library from Shared to Common

--- Common/Properties/AssemblyInfo.cs
+++ Common/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Shared")]
+[assembly: AssemblyTitle("Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Shared")]
+[assembly: AssemblyProduct("Common")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2bf10269-f7f8-432e-8a88-36d7c0364e16")]
+[assembly: Guid("e191f809-eb47-4e4f-8f10-485c7afa42f5")]
 
 // Version information for an assembly consists of the following four values:
 //
